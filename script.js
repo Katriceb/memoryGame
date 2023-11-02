54f475997bdd51ade461edb7792cb91819871cfe
Katriceb <birch5trice@gmail.com>
2023-11-02 14:42:50 -0400

    comment

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //const start = document.getElementById("button1") ;
-
+const memoryCard =document.querySelectorAll('.memory-card');
+const startbutton =document.getElementById('start-button');
+flipp 
 
 function flipp () {
 alert("start") 
 }
 //start.addEventListener("click",flipp);
 
+
+memoryCard.addEventListener("click",  myFunction);
